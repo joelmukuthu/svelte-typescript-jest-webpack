6311408e60edbdb00cdfa2e310192e8108fbe4d3
Joel Mukuthu <joelmukuthu@gmail.com>
2020-11-19 15:00:03 +0300

    chore: update Svelte & VSCode setup for Yarn 2

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,5 @@
 const autoPreprocess = require('svelte-preprocess');
 
 module.exports = {
-  preprocess: autoPreprocess({
-    typescript: {
-      tsconfigFile: './tsconfig.json',
-    }
-  })
+  preprocess: autoPreprocess()
 }
